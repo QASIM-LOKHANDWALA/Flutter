28e69a045e505cc81847a6a301875b8ce4f67ed4
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-02-22 14:47:30 +0530

    Login Animations

--- login/lib/pages/login_page.dart
+++ login/lib/pages/login_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:login/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +27,9 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 15,
               ),
-              const Text(
-                "Welcome",
-                style: TextStyle(
+              Text(
+                "Welcome $name",
+                style: const TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 28,
                 ),
@@ -37,6 +46,12 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter username",
                         labelText: "Username",
                       ),
+                      onChanged: (value){
+                        name = value;
+                        setState(() {
+                          
+                        });
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -48,16 +63,50 @@ class LoginPage extends StatelessWidget {
                     const SizedBox(
                       height: 15,
                     ),
-                    ElevatedButton(
-                      onPressed: (){
+                    // ElevatedButton(
+                    //   onPressed: (){
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   style: TextButton.styleFrom(
+                    //     minimumSize: Size(150, 50),
+                    //     backgroundColor: Colors.green,
+                    //     foregroundColor: Colors.white,
+                    //   ),
+                    //   child: const Text("LOGIN"),
+                    // ),
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+                        await Future.delayed(const Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
-                      style: TextButton.styleFrom(
-                        minimumSize: Size(150, 50),
-                        backgroundColor: Colors.green,
-                        foregroundColor: Colors.white,
+                      child: AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        width: changeButton?50:150,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          //shape: changeButton?BoxShape.circle:BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(changeButton?50:10),
+                          
+                        ),
+                        child: changeButton
+                          ? const Icon(
+                          Icons.done,
+                          color: Colors.white,
+                        )
+                          : const Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
                       ),
-                      child: const Text("LOGIN"),
                     ),
                   ],
                 ),
