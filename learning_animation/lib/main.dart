e0f200a5aff0f5d8061ecabaaade018f2eea6989
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-03-12 05:27:06 +0530

    Animated Color Pallete

--- learning_animation/lib/main.dart
+++ learning_animation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_animation/implicit_animation/animated_shopping_cart_button.dart';
 
 import 'implicit_animation/animated_color_pallete.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AnimatedColorPalette(),
+      home: const ShoppingCartButton(),
     );
   }
 }
