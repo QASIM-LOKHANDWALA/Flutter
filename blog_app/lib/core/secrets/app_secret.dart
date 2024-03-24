294b0fcb15f016f2185dc0d9b3a50f4c0a0c8e4c
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-03-24 14:56:05 +0530

    User Sign Up Implemented

---
+++ blog_app/lib/core/secrets/app_secret.dart
@@ -0,0 +1,4 @@
+class AppSecret {
+  static const supabaseURL = 'https://gzdimctgvfhhtmksmjfu.supabase.co';
+  static const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6ZGltY3RndmZoaHRta3NtamZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyNzA1NTQsImV4cCI6MjAyNjg0NjU1NH0.aaJ7S1ogOn-afD4kp0XMVvwa2ozPFT9Mnj2ruQsyR9I';
+}
