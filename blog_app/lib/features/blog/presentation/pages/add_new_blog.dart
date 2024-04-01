b7b81f8605cc542616334dccf66a776c931d74af
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2024-04-01 14:42:22 +0530

    New Blog Page UI

--- blog_app/lib/features/blog/presentation/pages/add_new_blog.dart
+++ blog_app/lib/features/blog/presentation/pages/add_new_blog.dart
@@ -1,6 +1,5 @@
-import 'dart:js';
-
 import 'package:blog_app/core/theme/AppPallete.dart';
+import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -14,6 +13,19 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
+
+  List<String> selectedTopics = [];
+
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    contentController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,69 +39,89 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
 
-            //  Image Selector
-            DottedBorder(
-              color: AppPallete.borderColor,
-              dashPattern: const [10, 4],
-              radius: const Radius.circular(10),
-              borderType: BorderType.RRect,
-              strokeCap: StrokeCap.round,
-              child: Container(
-                height: 150,
-                width: double.infinity,
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.folder_open,
-                      size: 40,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      'Select Your Image',
-                      style: TextStyle(
-                        fontSize: 15,
+              //  Image Selector
+              DottedBorder(
+                color: AppPallete.borderColor,
+                dashPattern: const [10, 4],
+                radius: const Radius.circular(10),
+                borderType: BorderType.RRect,
+                strokeCap: StrokeCap.round,
+                child: Container(
+                  height: 150,
+                  width: double.infinity,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.folder_open,
+                        size: 40,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 20,),
-
-            //  Categories
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  'Technology',
-                  'Business',
-                  'Programming',
-                  'Entertainment',
-                ]
-                    .map(
-                      (e) => Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Chip(
-                          label: Text(e),
-                          side: const BorderSide(color: AppPallete.borderColor,),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Select Your Image',
+                        style: TextStyle(
+                          fontSize: 15,
                         ),
                       ),
-                    )
-                    .toList(),
+                    ],
+                  ),
+                ),
               ),
-            ),
+
+              const SizedBox(height: 20,),
+
+              //  Categories
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    'Technology',
+                    'Business',
+                    'Programming',
+                    'Entertainment',
+                  ]
+                      .map(
+                        (e) => Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: GestureDetector(
+                            onTap: () {
+                              if(selectedTopics.contains(e)){
+                                selectedTopics.remove(e);
+                              } else {
+                                selectedTopics.add(e);
+                              }
+                              setState(() {});
+                            },
+                            child: Chip(
+                              color: selectedTopics.contains(e) ? const MaterialStatePropertyAll(AppPallete.gradient1) : null,
+                              label: Text(e),
+                              side: selectedTopics.contains(e) ? null : const BorderSide(color: AppPallete.borderColor,),
+                            ),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+
+              const SizedBox(height: 10,),
+
+              //  TextFields
+              BlogEditor(controller: titleController, hintText: 'Blog Title'),BlogEditor(controller: titleController, hintText: 'Blog Title'),
+              const SizedBox(height: 10,),
+              BlogEditor(controller: titleController, hintText: 'Blog Title'),BlogEditor(controller: titleController, hintText: 'Blog Title'),
 
 
-          ],
+            ],
+          ),
         ),
       ),
     );
